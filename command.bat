5cc2b115431c8e70846b10b93ac06617aee8e8b7
jmaddington <jm@jmaddington.com>
2018-10-15 11:37:31 -0400

    +

--- command.bat
+++ command.bat
@@ -1,3 +1,11 @@
-@ECHO OFF
+#Get UDF field to write to
+$udf = If ($env:udf -eq $null ) {"Custom3"} Else {$env:udf}
 
-Net user %cs_username% /DOMAIN /active:YES
+#Get current Windows version
+$ver = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").ReleaseId
+
+#Current version to stdout
+write-host $ver
+
+#write to UDF
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\CentraStage /v $udf /t REG_SZ /d $ver /f
